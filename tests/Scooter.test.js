96b32fa0fea21f81c564e47a1ed68ad40ec8eea1
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-10-21 13:06:18 +0100

    Add unit tests for scooter and user

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,88 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+describe("scooter object", () => {
+  beforeEach(() => {
+    newScooter = new Scooter();
+  });
+
+  test("scooter instantiates as expected", () => {
+    expect(newScooter).toBeInstanceOf(Scooter);
+  });
+
+  test("new scooter has station", () => {
+    expect(newScooter.station).toBeInstanceOf(String);
+  });
+
+  test("new scooter has serial", () => {
+    expect(newScooter.serial).toBeInstanceOf(Number);
+  });
+
+  test("new scooter is charged", () => {
+    expect(newScooter.charged).toBeInstanceOf(Number);
+  });
+
+  test("new scooter is not broken", () => {
+    expect(newScooter.isBroken).toBe(false);
+  });
+
+  test("new scooter is docked", () => {
+    expect(newScooter.docked).toBe(true);
+  });
+});
 
 //Method tests
-describe('scooter methods', () => {
-  // tests here!
+describe("scooter methods", () => {
+  beforeEach(() => {
+    anotherScooter = new Scooter();
+  });
 
-  //rent method
+  afterEach(() => {
+    anotherScooter = new Scooter();
+  });
 
-  //dock method
+  test("Check rent method works with correct values", () => {
+    anotherScooter.isBroken = false;
+    anotherScooter.charge = 50;
+    jest.spyOn(console.log());
+    anotherScooter.rent();
+    expect(console.log).toHaveBeenCalledWith("Enjoy the ride!");
+    expect(anotherScooter.docked).toBe(false);
+  });
 
-  //requestRepair method
+  test("check rent method works appropriately when scooter broken", () => {
+    anotherScooter.isBroken = true;
+    expect(anotherScooter.rent()).toThrow(
+      "Scooter is broken, please send a repair request"
+    );
+  });
 
-  //charge method
+  test("check rent method works appropriately when scooter is not charged", () => {
+    anotherScooter.charged = 19;
+    expect(anotherScooter.rent()).toThrow(
+      "Scooter low on battery, please charge."
+    );
+  });
 
-})
+  test("Check scooter can recharge", async () => {
+    anotherScooter.charged = 30;
+    await anotherScooter.recharge();
+    expect(anotherScooter.charged).toBe(100);
+  });
+
+  test("Check dock function sets scooter location when provided", () => {
+    anotherScooter.dock("Haddock");
+    expect(anotherScooter.location).toBe("Haddock");
+  });
+
+  test("Check dock function throws error when no location given", () => {
+    anotherScooter.dock();
+    expect(anotherScooter.location).toThrow("Docking station required!");
+  });
+});
+
+//   //dock method
+//   //requestRepair method
+//   //charge method
+// });
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,9 +1,24 @@
-const User = require('../src/User')
+const User = require("../src/User");
 
 // User tests here
+describe("Tests user class for key properties", () => {
+  beforeEach(() => {
+    newUser = new User("user123", "password", 17);
+  });
 
-// test username
-
-// test password
-
-// test age
+  test("Object created is instance of user.", () => {
+    expect(newUser).toBeInstanceOf(User);
+  });
+  // test username
+  test("Test username has been set correctly", () => {
+    expect(newUser.username).toBe("user123");
+  });
+  // test password
+  test("Test password has been set correctly", () => {
+    expect(newUser.password).toBe("password");
+  });
+  // test age
+  test("Test age has been set correctly", () => {
+    expect(newUser.age).toBe(17);
+  });
+});
